9b388b187e9cc8282344073c86723d41ce13683f
kwisnia <230037@edu.p.lodz.pl>
2024-12-10 20:09:29 +0100

    day 4

--- day4/day4.py
+++ day4/day4.py
@@ -50,10 +50,33 @@ def word_check(
     return matches_found
 
 
+def x_word_check(
+    word: str,
+    letter_map: dict[tuple[int, int], str],
+    starting_position: tuple[int, int],
+) -> bool:
+    if len(word) == 1:
+        return 1
+    if len(word) % 2 == 0:
+        raise ValueError("Word length must be odd")
+    x, y = starting_position
+    first_axis_word = ""
+    second_axis_word = ""
+    axis_length = len(word) // 2
+    for i in range(-axis_length, axis_length + 1):
+        first_axis_word += letter_map.get((x + i, y + i), "")
+        second_axis_word += letter_map.get((x + i, y - i), "")
+
+    return (first_axis_word == word or first_axis_word[::-1] == word) and (
+        second_axis_word == word or second_axis_word[::-1] == word
+    )
+
+
 if __name__ == "__main__":
     with open("day4/day4.txt") as f:
         # letter_map = parse_input(test_input.strip().split("\n"))
         letter_map = parse_input(f.read().strip().split("\n"))
+        # Part 1
         positions_with_letter_x = [
             (x, y) for (x, y), letter in letter_map.items() if letter == "X"
         ]
@@ -61,5 +84,17 @@ if __name__ == "__main__":
             "Part 1 answer: ",
             sum(
                 [word_check("XMAS", letter_map, pos) for pos in positions_with_letter_x]
+            ),
+        )
+        positions_with_letter_a = [
+            (x, y) for (x, y), letter in letter_map.items() if letter == "A"
+        ]
+        print(
+            "Part 2 answer: ",
+            sum(
+                [
+                    x_word_check("MAS", letter_map, pos)
+                    for pos in positions_with_letter_a
+                ]
             ),
         )
