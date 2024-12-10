058b0bf2924b4d339ef1fe9ad8e12caa920fba5e
kwisnia <230037@edu.p.lodz.pl>
2024-12-10 13:05:12 +0100

    optimise day 10 solution

--- day10/day10.py
+++ day10/day10.py
@@ -9,31 +9,23 @@ test_input = """
 10456732
 """
 
+
 def move(
     map: list[list[int]],
     position: tuple[int, int],
-    count_not_unique_paths: bool = False,
     previous_position: tuple[int, int] = None,
-    current_tree: dict[tuple[int, int], dict[str, tuple[int, int] | int]] = None,
+    node_history: list[str, dict[str, tuple[int, int] | int]] = None,
 ):
-    if current_tree is None:
-        if count_not_unique_paths:
-            current_tree = [
-                {
-                    "position": position,
-                    "parent": None,
-                    "level": map[position[0]][position[1]],
-                }
-            ]
-        else:
-            current_tree = {
-                position: {
-                    "parent": None,
-                    "level": map[position[0]][position[1]],
-                }
-            }
     current_y, current_x = position
     level = map[current_y][current_x]
+    if node_history is None:
+        node_history = [
+            {
+                "position": position,
+                "level": level,
+            }
+        ]
+
     potential_moves = []
     if current_y - 1 >= 0:
         potential_moves.append((current_y - 1, current_x))
@@ -49,42 +41,27 @@ def move(
         y, x = potential_move
         if map[y][x] == level + 1:
             node = {
-                "parent": position,
                 "level": level + 1,
+                "position": potential_move,
             }
-            if count_not_unique_paths:
-                node["position"] = potential_move
-                current_tree.append(node)
-                move(
-                    map, potential_move, count_not_unique_paths, position, current_tree
-                )
-            else:
-                if potential_move not in current_tree:
-                    current_tree[potential_move] = node
-                    if level + 1 < 9:
-                        move(
-                            map,
-                            potential_move,
-                            count_not_unique_paths,
-                            position,
-                            current_tree,
-                        )
+            node_history.append(node)
+            if level + 1 < 9:
+                move(map, potential_move, position, node_history)
 
-    return current_tree
+    return node_history
 
 
-def calculate_level_9_tree_width(
-    tree: dict[tuple[int, int], dict[str, tuple[int, int] | int]]
-    | list[dict[str, tuple[int, int] | int]]
+def calculate_level_9_nodes(
+    places: list[dict[str, tuple[int, int] | int]],
+    count_not_unique_paths: bool = False,
 ):
-    if isinstance(tree, list):
-        level_9_positions = [node for node in tree if node["level"] == 9]
+    if count_not_unique_paths:
+        return len([node for node in places if node["level"] == 9])
     else:
-        level_9_positions = [
-            position for position in tree if tree[position]["level"] == 9
-        ]
-
-    return len(level_9_positions)
+        level_9_unique_positions = {
+            node["position"] for node in places if node["level"] == 9
+        }
+        return len(level_9_unique_positions)
 
 
 def print_tree(tree: list[dict]):
@@ -108,12 +85,13 @@ if __name__ == "__main__":
         # ]
         map = [[int(x) for x in line.strip()] for line in f]
         starting_positions = find_starting_positions(map)
+        possible_paths = [move(map, position) for position in starting_positions]
         print(
             "Part 1 answer:",
             sum(
                 [
-                    calculate_level_9_tree_width(move(map, position))
-                    for position in starting_positions
+                    calculate_level_9_nodes(path, count_not_unique_paths=False)
+                    for path in possible_paths
                 ]
             ),
         )
@@ -121,10 +99,8 @@ if __name__ == "__main__":
             "Part 2 answer:",
             sum(
                 [
-                    calculate_level_9_tree_width(
-                        move(map, position, count_not_unique_paths=True)
-                    )
-                    for position in starting_positions
+                    calculate_level_9_nodes(path, count_not_unique_paths=True)
+                    for path in possible_paths
                 ]
             ),
         )
